854d7598800eb4d5b95640640e2d0654af6de90c
kirillaccount <kitilltremasov251206@gmail.com>
2025-06-24 22:38:31 +0300

    [tremasov] provverka svazi

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import os
 
-
+'''
 def main() -> None:
     # ...
     pass
+'''
 
+class TreeNode:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
 
+def insert_into_tree(root, value):
+    if root is None:
+        return TreeNode(value)
+    if value < root.value:
+        root.left = insert_into_tree(root.left, value)
+    else:
+        root.right = insert_into_tree(root.right, value)
+    return root
+
+def main():
+    input_filename = 'unsorted_nums.txt'
+    output_filename = 'sorted_nums.txt'
+'''
 if __name__ == '__main__':
     main()
-
+'''
 
